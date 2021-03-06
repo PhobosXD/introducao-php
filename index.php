df2f2742ad3a5e2478148083d1811a9917b6269a
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-03-06 13:33:55 -0300

    Condicionais

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
+
 $categorias = [];
 
 $categorias[] = 'Infantil';
 $categorias[] = 'Adolescente';
 $categorias[] = 'Adulto';
 
-print_r($categorias);
+#print_r($categorias);
+
+$nome = 'Cazuza';
+$idade = 20;
+
+#var_dump($nome);
+#var_dump($idade);
+
+if ($idade >= 6 && $idade <= 12) {
+    print($categorias[0]);
+} else if ($idade > 12 && $idade <= 18) {
+    print($categorias[1]);
+} else if ($idade > 18) {
+    print($categorias[2]);
+} else {
+    echo('Idade inválida!');
+}
+
 ?>
