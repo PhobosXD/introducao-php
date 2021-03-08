18caa7400c91c6907a15bcb9e58e4ec8c536c38e
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-03-07 21:23:09 -0300

    Validando nome, idade e mensagens de sessão

--- services/Session-message.php
+++ services/Session-message.php
@@ -10,4 +10,24 @@ function get_error_message() : ?string {
     return isset($_SESSION['error-message']) ? $_SESSION['error-message'] : NULL;
 }
 
+function set_sucess_message(string $message) : void {
+    $_SESSION['sucess-message'] = $message;
+}
+
+function get_sucess_message() : ?string {
+    return isset($_SESSION['sucess-message']) ? $_SESSION['sucess-message'] : NULL;
+}
+
+function delete_error_message() : void {
+    if (isset($_SESSION['error-message'])) {
+        unset($_SESSION['error-message']);
+    }
+}
+
+function delete_sucess_message() : void {
+    if (isset($_SESSION['sucess-message'])) {
+        unset($_SESSION['sucess-message']);
+    }
+}
+
 ?>
